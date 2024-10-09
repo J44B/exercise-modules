135bfcaf97eeb3176a0356c81c2bd26a705f3f10
jochen <jochen@byte23.net>
2024-10-09 13:06:20 +0200

    redesign everything, not only storage

--- fakey-storey/modules/storage.js
+++ fakey-storey/modules/storage.js
@@ -1,1 +1,7 @@
 // For utilizing local or session storage
+
+export function addToLocalStorage(key, value) {
+  const data = JSON.parse(localStorage.getItem(key)) || [];
+  data.push(value);
+  localStorage.setItem(key, JSON.stringify(data));
+}
